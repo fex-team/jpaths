7ebcb8d55777154f1851a9aee89f64daec36bb39
worry127722 <worry127722@126.com>
2014-12-16 14:20:38 +0800

    test

---
+++ _test/case/jpaths.js
@@ -0,0 +1,53 @@
+/**
+ * Created by wangrui10 on 14-12-16.
+ */
+module( 'case/jpaths' );
+
+test('列表内没有列表标号的项后退', function () {
+        ok(true,"123");
+    define('start', function(require) {
+        var jpaths = require('jpaths');
+        var path = jpaths(['m', 0, 0, 'A', 10, 10, 0, 0, 0, 10, 102]);
+        // var path = jpaths();
+        //
+        window.path = path;
+        var x = path.toString();
+        console.log(path.toString()); // [['M', 0, 0], ['h', 3], ['v', 4], ['L', 5, 6]]
+        equal(x, "m0,0A10,10,0,0,0,10,102", "标记名称1");
+    });
+    require(['start'], function() {
+        console.log('start');
+    });
+    stop();
+    setTimeout(function(){
+        var xs = "m0,0A10,10,0,0,0,10,102";
+        equal(xs, "m0,0A10,10,0,0,0,10,102", "标记名称2");
+        start();
+    },1000);
+
+});
+
+
+test('列表内没有列表标号的项后退', function () {
+    ok(true,"123");
+    define('start', function(require) {
+        var jpaths = require('jpaths');
+        var path = jpaths(['m', 0, 0, 'A', 10, 10, 0, 0, 0, 10, 10]);
+        // var path = jpaths();
+        //
+        window.path = path;
+        var x = path.toString();
+        console.log(path.toString()); // [['M', 0, 0], ['h', 3], ['v', 4], ['L', 5, 6]]
+        equal(x, "m0,0A10,10,0,0,0,10,10", "标记名称1");
+    });
+    require(['start'], function() {
+        console.log('start');
+    });
+    stop();
+    setTimeout(function(){
+        var xs = "m0,0A10,10,0,0,0,10,10";
+        equal(xs, "m0,0A10,10,0,0,0,10,10", "标记名称2");
+        start();
+    },1000);
+
+});
--- esl.js
+++ esl.js
@@ -3,25 +3,46 @@
  */
 
 
-asyncTest('列表内没有列表标号的项后退', function () {
+asyncTest('测试 .toString()方法', function () {
 
-    define('start', function(require) {
-        var jpaths = require('jpaths');
-        var path = jpaths(['m', 0, 0, 'A', 10, 10, 0, 0, 0, 10, 10]);
+    define('start1', function(require) {
+        var jpaths = require('src/jpaths');
+        var path = jpaths(['M', 0, 0, 'A', 10, 10, 0, 0, 0, 10, 10]);
         // var path = jpaths();
-        //
-        window.path = path;
         var x = path.toString();
-        console.log(path.toString()); // [['M', 0, 0], ['h', 3], ['v', 4], ['L', 5, 6]]
-        equal(x, "m0,0A10,10,0,0,0,10,10", "标记名称1");
-    });
-    require(['start'], function() {
-        console.log('start');
-    });
-    setTimeout(function(){
-        var xs = "m0,0A10,10,0,0,0,10,10";
-        equal(xs, "m0,0A10,10,0,0,0,10,10", "标记名称2");
+        console.log(x); // [['M', 0, 0], ['h', 3], ['v', 4], ['L', 5, 6]]
+        equal(x, "M0,0A10,10,0,0,0,10,10", ".toString()");
 
-    },1000);
+        x = path.toString('%s');
+        console.log(x);
+        equal(x, 'M 0,0 A 10 10 0 0 0 10,10', ".toString('%s')");
 
+        x = path.toString('%n');
+        console.log(x);
+        equal(x, 'M 0,0\nA 10 10 0 0 0 10,10', ".toString('%n')");
+    });
+    require(['start1'], function() {
+        console.log('start1');
+    });
 });
+
+asyncTest('测试 .toArray() / .valueOf() 方法', function () {
+
+    equal('hhlkh', 'sfsfsfsff', ".valueOf()");
+    // define('start2', function(require) {
+    //     var jpaths = require('jpaths');
+    //     var path = jpaths('M0,0h3v4L5,6');
+    //     // var path = jpaths();
+    //     //
+    //     var x = path.toArray();
+    //     console.log(x); // [['M', 0, 0], ['h', 3], ['v', 4], ['L', 5, 6]]
+    //     equal(x, [['M', 0, 0], ['h', 3], ['v', 4], ['L', 5, 6]], ".toArray()");
+
+    //     x = path.valueOf();
+    //     equal(x, [['M', 0, 0], ['h', 3], ['v', 4], ['L', 5, 6]], ".valueOf()");
+    // });
+    // require(['start2'], function() {
+    //     console.log('start2');
+    // });
+});
+
