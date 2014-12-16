41ab46c9db47f0d9ecd7c3ee4ea2c37466da2a6c
worry127722 <worry127722@126.com>
2014-12-16 15:34:09 +0800

    [test]

--- _test/case/jpaths.js
+++ _test/case/jpaths.js
@@ -4,18 +4,15 @@
 module( 'case/jpaths' );
 
 test('列表内没有列表标号的项后退', function () {
-        ok(true,"123");
-    define('start', function(require) {
-        var jpaths = te_jpaths;
+    define('start1', function(require) {
+        var jpaths = require('jpaths');
         var path = jpaths(['m', 0, 0, 'A', 10, 10, 0, 0, 0, 10, 102]);
-        // var path = jpaths();
-        //
         window.path = path;
         var x = path.toString();
         console.log(path.toString()); // [['M', 0, 0], ['h', 3], ['v', 4], ['L', 5, 6]]
         equal(x, "m0,0A10,10,0,0,0,10,102", "标记名称1");
     });
-    require(['start'], function() {
+    require(['start1'], function() {
         console.log('start');
     });
     stop();
@@ -24,23 +21,19 @@ test('列表内没有列表标号的项后退', function () {
         equal(xs, "m0,0A10,10,0,0,0,10,102", "标记名称2");
         start();
     },100);
-
 });
 
 
 test('列表内没有列表标号的项后退', function () {
-    ok(true,"123");
-    define('start', function(require) {
+    define('start2', function(require) {
         var jpaths = require('jpaths');
         var path = jpaths(['m', 0, 0, 'A', 10, 10, 0, 0, 0, 10, 10]);
-        // var path = jpaths();
-        //
         window.path = path;
         var x = path.toString();
         console.log(path.toString()); // [['M', 0, 0], ['h', 3], ['v', 4], ['L', 5, 6]]
         equal(x, "m0,0A10,10,0,0,0,10,10", "标记名称1");
     });
-    require(['start'], function() {
+    require(['start2'], function() {
         console.log('start');
     });
     stop();
@@ -48,6 +41,25 @@ test('列表内没有列表标号的项后退', function () {
         var xs = "m0,0A10,10,0,0,0,10,10";
         equal(xs, "m0,0A10,10,0,0,0,10,10", "标记名称2");
         start();
-    },1000);
+    },100);
+});
 
+test('列表内没有列表标号的项后退', function () {
+    define('start3', function(require) {
+        var jpaths = require('jpaths');
+        var path = jpaths(['m', 0, 0, 'A', 10, 10, 0, 0, 0, 10, 10]);
+        window.path = path;
+        var x = path.toString();
+        console.log(path.toString()); // [['M', 0, 0], ['h', 3], ['v', 4], ['L', 5, 6]]
+        equal(x, "m0,0A10,10,0,0,0,10,10", "标记名称1");
+    });
+    require(['start3'], function() {
+        console.log('start');
+    });
+    stop();
+    setTimeout(function(){
+        var xs = "m0,0A10,10,0,0,0,10,10";
+        equal(xs, "m0,0A10,10,0,0,0,10,10", "标记名称2");
+        start();
+    },100);
 });
